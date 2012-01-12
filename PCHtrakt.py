d368a7bc853904cf76cb37b600f6cc1e3e885b30
kedalel <jonathan.lauwers@gmail.com>
2012-01-12 22:29:15 +0100

    GNU licence

--- PCHtrakt.py
+++ PCHtrakt.py
@@ -1,3 +1,22 @@
+# -*- coding: utf-8 -*-
+# Authors: Jonathan Lauwers / Frederic Haumont
+# URL: http://github.com/PCHtrakt/PCHtrakt
+#
+# This file is part of PCHtrakt.
+#
+# PCHtrakt is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PCHtrakt is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PCHtrakt.  If not, see <http://www.gnu.org/licenses/>.
+
 import sys 
 
 from pchRequest import *
@@ -5,7 +24,6 @@ from config import *
 from time import sleep
 from utilities import *
 import getopt
-import parser
 global stop
 stop = 0
 
@@ -21,8 +39,6 @@ def printHelp():
 	print 'TODO'
 
 def main():
-	np = parser.NameParser(True)
-	parse_result = np.parse('test - 1x06 - toto.mkv')
 	reqPch = PchRequest(ipPch)
 	oStatus = reqPch.getPchStatus()	
 	if oStatus.status != EnumStatus.NOPLAY:
