817e5e9b51a4d32c2aaef5507222a44146ecbc8e
jlauwers <jonathan.lauwers@gmail.com>
2012-02-09 19:36:42 +0100

    betaseries api

--- pchtrakt/betaseries.py
+++ pchtrakt/betaseries.py
@@ -4,17 +4,14 @@ from xml.etree import ElementTree
 from HTMLParser import HTMLParser
 from hashlib import md5
 
-BETASERIE_URL = 'http://api.betaseries.com'
 BETASERIE_API = 'C244D9326837'
-
-LOGIN = 'Dev023'
-PASS = 'developer'
+BetaSerieUrl = 'http://api.betaseries.com/#path#?key={0}'.format(BETASERIE_API)
+                                        
+def getUrl(myPath):
+    return BetaSerieUrl.replace('#path#', myPath)
 
 def destroyToken(Token):
-    url = '{0}/members/destroy.xml?key={1}&token={2}'.format(
-                        BETASERIE_URL,
-                        BETASERIE_API,
-                        Token)
+    url = getUrl('members/destroy.xml') + '&token={0}'.format(Token)
     oResponse = urlopen(url)
     oXml = ElementTree.XML(oResponse.read())
     if oXml.find('code').text == '1':
@@ -24,59 +21,51 @@ def destroyToken(Token):
 
 def getSerieUrl(SerieName):
     SerieName = quote(SerieName)
-    url = '{0}/shows/search.xml?key={1}&title={2}'.format(
-                        BETASERIE_URL,
-                        BETASERIE_API,
-                        SerieName)
-    print(url)
+    url = getUrl('shows/search.xml') + '&title={0}'.format(SerieName)
     oResponse = urlopen(url)
     oXml = ElementTree.XML(oResponse.read()) 
     return quote('{0}.xml'.format(oXml.find("shows/show/url").text))
 
-def getToken(log,passw):
-    url = '{0}/members/auth.xml?key={1}&login={2}&password={3}'.format(
-                        BETASERIE_URL,
-                        BETASERIE_API,
-                        log,
-                        md5(passw).hexdigest())
+def getToken(login,password):
+    url = (getUrl('members/auth.xml') 
+                + '&login={0}&password={1}'.format(
+                                                login,
+                                                md5(password).hexdigest()))
     oResponse = urlopen(url)
     oXml = ElementTree.XML(oResponse.read()) 
     return oXml.find("member/token").text
     
 def scrobbleEpisode(SerieXml,Token,Saison,Episode):
     #todo(jlauwers) check before if this episode is already seen.
-    url = '{0}/members/watched/{1}?key={2}&season={3}&episode={4}&token={5}'.format(
-                        BETASERIE_URL,
-                        SerieXml,
-                        BETASERIE_API,
-                        Saison,
-                        Episode,
-                        Token)
+    url = getUrl('members/watched/{0}'.format(SerieXml))
+    url += '&season={0}&episode={1}&token={2}'.format(
+                                                Saison,
+                                                Episode,
+                                                Token)
     oResponse = urlopen(url)
     oXml = ElementTree.XML(oResponse.read())
+    
     if oXml.find("code").text == '1':
         return True
     else:
         pass
-        #todo(jlauwers) this user don't watch this show
+        #todo(jlauwers) error message
 
 def addShow(SerieXml,Token):
-    url = '{0}/shows/add/{1}?key={2}&token={3}'.format(
-                        BETASERIE_URL,
-                        SerieXml,
-                        BETASERIE_API,
-                        Token)
+    url = getUrl('shows/add/{0}'.format(SerieXml)) + '&token={0}'.format(Token)
     oResponse = urlopen(url)
     oXml = ElementTree.XML(oResponse.read())
     if oXml.find("code").text == '1':
         return True
     else:
         return oXml.find("errors/error/content").text
-        
-# SerieXml = getSerieUrl('Sherlock')
-# print SerieXml
-# userToken = userAuth(LOGIN,PASS)
-# print userToken
-# print addShow(SerieXml,userToken)
-# print scrobbleEpisode(SerieXml,userToken,2,1)
-# destroyToken(userToken)
+
+def isEpisodeWatched(SerieXml,Token,Saison,Episode):
+    url = getUrl('shows/episodes/{0}'.format(SerieXml))
+    url += '&token={0}&season={1}&episode={2}'.format(Token,Saison,Episode)
+    oResponse = urlopen(url)
+    oXml = ElementTree.XML(oResponse.read())
+    if oXml.find("seasons/season/episodes/episode/has_seen").text == 1:
+        return True
+    else:
+        return False
