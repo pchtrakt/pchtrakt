263f4b033d760d359c02ba573438dab6894e7560
jlauwers <jonathan.lauwers@gmail.com>
2012-02-20 21:20:09 +0100

    movie library checking

--- pchtrakt/library.py
+++ pchtrakt/library.py
@@ -19,8 +19,15 @@
 
 import os
 import mediaparser as mp
+from movieparser import MovieResultNotFound
+from json import dump
+from urllib import quote
+from urllib2 import urlopen
+from xml.etree import ElementTree
+from xml.parsers.expat import ExpatError
 from lib import utilities as utils
 from lib.tvdb_api import tvdb_api 
+
 
 mediaparser = mp.MediaParser()
 tvdb = tvdb_api.Tvdb()
@@ -28,10 +35,16 @@ tvdb = tvdb_api.Tvdb()
 class library:
     def add(self, path):
         mySerieDict = {}
+        myMovieList = []
         for root, subFolders, files in os.walk(path):
             for file in files:
-                if not file.endswith('srt'):
-                    parsedInfo = mediaparser.parse(file)
+                if (file.endswith('mkv') or file.endswith('avi') or file.endswith('mp4')):
+                    utils.Debug(file)
+                    try:
+                        parsedInfo = mediaparser.parse(file)
+                    except MovieResultNotFound:
+                        utils.Debug('Error for file %s' % file)
+                        continue
                     if isinstance(parsedInfo,mp.MediaParserResultTVShow):
                         for episode in parsedInfo.episode_numbers:
                             myEpisode = { 'season': parsedInfo.season_number, 
@@ -41,14 +54,33 @@ class library:
                             else:
                                 mySerieDict[parsedInfo.series_name] = [myEpisode]
                     elif isinstance(parsedInfo,mp.MediaParserResultMovie):
-                        pass
-                        
-        for serie, episodes in mySerieDict.iteritems():
-            id = tvdb[serie]['id']
-            year = tvdb[serie]['firstaired'].split('-')[0]
-            episodes = sorted(episodes, key=lambda student: student['episode'])
-            episodes = sorted(episodes, key=lambda student: student['season'])
-            utils.setEpisodesInLibraryOnTrakt(id,serie,year,episodes)
-
-
+                        ImdbAPIurl = ('http://www.imdbapi.com/?t={0}&y={1}&r=xml'
+                                            .format(quote(parsedInfo.movie_title),
+                                            parsedInfo.year))
+                        oResponse = urlopen(ImdbAPIurl)
+                        try:   
+                            oXml = ElementTree.XML(oResponse.read())
+                        except ExpatError: 
+                            utils.Debug('Error for movie %s' % (parsedInfo.movie_title))
+                            continue
+                        id = oXml.find('movie').get('id')
+                        matchedTitle = oXml.find('movie').get('title')
+                        utils.Debug('Matching %s:' % parsedInfo.movie_title)
+                        myMovie = { 'imdb_id': id,
+                                    'title': matchedTitle,
+                                    'year': parsedInfo.year }
+                        myMovieList.append(myMovie)
+                        print myMovie
+        
+        # for serie, episodes in mySerieDict.iteritems():
+            # id = tvdb[serie]['id']
+            # year = tvdb[serie]['firstaired'].split('-')[0]
+            # episodes = sorted(episodes, key=lambda student: student['episode'])
+            # episodes = sorted(episodes, key=lambda student: student['season'])
+            # utils.setEpisodesInLibraryOnTrakt(id,serie,year,episodes)
+   
+        
+        f = open('output.txt', 'w')
+        dump(myMovieList, f)
+        f.close()
     
