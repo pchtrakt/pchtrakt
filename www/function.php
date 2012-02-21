0ddd2c986081f000402812c8e26f50e26cdcd353
Damien <d.chianura@googlemail.com>
2012-02-21 20:35:43 +0100

    Add _bool strict function, not a good night

--- www/function.php
+++ www/function.php
@@ -35,5 +35,28 @@ if (!defined('PCHTRAKT'))
 	function _execPy()
 	{
 		exec('cd /share/Apps/pchtrakt && ./daemon.sh restart');
-	}		
+	}	
+	
+	function _bool($in, $strict=false) 
+	{
+		$out = null;
+		// if not strict, we only have to check if something is false
+		if (in_array($in,array('false', 'False', 'FALSE', 'no', 'No', 'n', 'N', '0', 'off','Off', 'OFF', false, 0, null), true)) 
+		{
+			$out = false;
+		} 
+		else if ($strict) 
+		{
+			// if strict, check the equivalent true values
+			if (in_array($in,array('true', 'True', 'TRUE', 'yes', 'Yes', 'y', 'Y', '1','on', 'On', 'ON', true, 1), true)) 
+				$out = true;
+
+		} 
+		else 
+		{
+			// not strict? let the regular php bool check figure it out (will largely default to true)
+			$out = ($in?true:false);
+		}
+		return $out;
+	}
 ?>
